b3ecd8312b61cca480a582518f3f122fcd86207e
Colby Tomita <cotomita808@gmail.com>
2024-04-17 11:09:04 -0700

    created game.php and shell code

---
+++ game.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Finish the Lyrics!</title>
+</head>
+<body>
+    <?php
+    $DBName="";
+    $dbc = @mysqli_connect("", "admin", "password", $DBName);
+    $sql = "";
+    $rs  = mysqli_query($dbc, $sql);
+    echo "<h1>Round " . $_GET['round'] . "</h1>";
+    ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,8 +3,10 @@
     <title>Finish the Lyrics!</title>
 </head>
 <body>
-    <?php
-        echo "Hello World!";
-    ?>
+    <h1>This is a game to finish the lyrics of a song!</h1>
+    <p>Click the button below to start the game!</p>
+    <form action="game.php" method="post">
+        <input type="submit" value="Start Game">
+    </form>
 </body>
 </html>
